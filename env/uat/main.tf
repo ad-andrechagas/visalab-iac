f9115ae9a4ad8f3136e205b04dfb9719719c7157
Chagas, Andre <alm2.servicos.ti@gmail.com>
2023-09-19 14:36:13 -0300

    added code for future environments

---
+++ env/uat/main.tf
@@ -0,0 +1,51 @@
+# Visalab IAC Development Environment #
+#######################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "lab-terraform-state-bucket"
+    key     = "uat/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
+
+# =================================================================
+# This is optional as it is for giving a randomic name in cloud.
+resource "random_pet" "this" {
+  length = 2
+}
+# =================================================================
+
+module "visa-bucket" {
+  # source = "../../../tf-modules/dev/s3/"
+  source = "git@github.com:ad-andrechagas/tf-module-s3.git"
+  name   = "visalab-${random_pet.this.id}"
+  tags = {
+    Name        = "VisaLab Bucket"
+    Environment = "DEV"
+    Managedby   = "VisaLab Team"
+  }
+
+  versioning = {
+    enabled = true
+  }
+}
+# Under construction
+# module "visa-rds" {
+#  source = "git@github.com:ad-andrechagas/tf-module-rds.git"
+#}
