0b7e4b163416c5e88ae3f27ce1994a0be69e6021
Andre Chagas <143511353+ad-andrechagas@users.noreply.github.com>
2023-09-28 23:07:51 -0300

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,13 +1,3 @@
-# provider "aws" {
-#   region = local.region
-
-#   # Make it faster by skipping something
-#   skip_metadata_api_check     = true
-#   skip_region_validation      = true
-#   skip_credentials_validation = true
-#   skip_requesting_account_id  = true
-# }
-
 # Visalab IAC Development Environment #
 #######################################
 
